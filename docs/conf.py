bb8f545fc03309c4a51b3219a90dc010f7e684fe
Anatoli <afedynitch@gmail.com>
2015-02-16 17:33:43 +0100

    Fixed conf.py using mock

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 import mock
  
 MOCK_MODULES = ['numpy', 'scipy','scipy.optimize']
-for mod_name in MOCK_MODULES:
+for mod_name in MOCK_MODULES: 
     sys.modules[mod_name] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
